95d976fe2754f4cee1ca77145433a78c1c2f0fdf
unknown <dheepakyadav2411@gmail.com>
2025-09-10 23:13:00 -0400

    dns

---
+++ std-socket.py
@@ -0,0 +1,6 @@
+import socket
+
+# Performs a standard system-level DNS lookup
+hostname = 'example.com'
+ip_address = socket.gethostbyname(hostname)
+print(f"The IP address of {hostname} is {ip_address}")
